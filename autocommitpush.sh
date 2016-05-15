e39b10ad2015958f6418544a786a589f1d8fcf86
Heena <heena@ivp.in>
2016-05-16 01:10:54 +0530

    pushedfrombashscriptmail

--- autocommitpush.sh
+++ autocommitpush.sh
@@ -4,7 +4,7 @@ git add --all && git commit -m $1 && git push origin $2
 
 TO_ADDRESS="heena393@gmail.com"
 FROM_ADDRESS="heena393@gmail.com"
-SUBJECT="Commit digest - $1 : Pushed to $2" 
+SUBJECT="Commit digest - $1 Pushed to $2" 
 BODY="This is a automated mail. Please do not reply. "
 
 echo ${BODY}| mail -s ${SUBJECT} ${TO_ADDRESS} -- -r ${FROM_ADDRESS}
